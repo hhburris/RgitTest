2560cea7f4322101596bd3ce1c41019233e8e19f
hhburris <hherson@gmail.com>
2014-01-28 12:05:20 -0500

    miRNA data in, but need to remember this doesn't take batches into account.

--- code/nanostring1_import.R
+++ code/nanostring1_import.R
@@ -3,5 +3,35 @@
 # Started 1/27/2014
 
 library(NanoStringNorm)
+library(gdata)
+installXLSXsupport()
+installXLSXsupport(perl = "C:/Strawberry/perl/bin/perl.exe")
 
+
+path.data <- "C:/Users/heburris/Dropbox/Cervix paper/RNA/RCC/miRNARCC"
+list.files(file.path(path.data))
+path.to.xls.file <- system.file( "RCCCollector_miRNA.xls",
+package = "NanoStringNorm")
+NanoString.mRNA <- read.xls.RCC(x = file.path(path.data, "RCCCollector_miRNA.xlsx"),  sheet = 1)
+df1<-NanoString.mRNA <- read.xls.RCC(x = file.path(path.data, "RCCCollector_miRNA.xlsx"),  sheet = 1)
+df1
+str(df1)
+NanoString.mRNA.norm <- NanoStringNorm(
+x = NanoString.mRNA,
+anno = NA,
+CodeCount = 'sum',
+Background = 'mean',
+SampleContent = 'housekeeping.sum',
+round.values = FALSE,
+take.log = FALSE,
+return.matrix.of.endogenous.probes = TRUE
+);
+NanoString.mRNA.norm <- NanoStringNorm(
+x = NanoString.mRNA,
+CodeCount = 'geo.mean',
+Background = 'mean.2sd',
+SampleContent = 'housekeeping.geo.mean',
+round.values = TRUE,
+take.log = TRUE
+);
 
